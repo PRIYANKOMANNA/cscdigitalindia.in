e732f8705870b9d2f1a1d2bd6d9f46a912e4dbd8
Priyanko Manna <priyanko.rishi@gmail.com>
2025-09-13 15:11:58 +0530

    q
    q

---
+++ js/sitePosts.js
@@ -0,0 +1,3 @@
+<!-- head এ রাখো যেন main HTML এর স্ক্রিপ্ট চলার আগে সেটি উপস্থিত থাকে -->
+<script src="js/sitePosts.js"></script>
+<!-- তারপর তোমার main index.html বা ব্লগ স্ক্রিপ্ট যা আমি দিলাম -->
